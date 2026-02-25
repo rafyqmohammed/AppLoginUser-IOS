405cb5891a810b6d8f97123be804f9534d3c0016
rafyqmohammed <rafyq.mohammed.111@gmail.com>
2026-02-25 12:49:17 +0000

    Initial commit

---
+++ AppLoginUser/AppLoginUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLoginUserApp.swift
+//  AppLoginUser
+//
+//  Created by tamtam-8 on 25/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct AppLoginUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppLoginUser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppLoginUser
+//
+//  Created by tamtam-8 on 25/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
